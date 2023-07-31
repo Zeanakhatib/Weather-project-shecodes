3907491010e6fc8f567180a01f4c4efbfa57eca3
Zeanakhatib <109243728+Zeanakhatib@users.noreply.github.com>
2023-07-30 21:14:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,47 @@
-function formatDate(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
+document.addEventListener("DOMContentLoaded", function () {
+  function formatDate(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
   }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
+
+  function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
   }
 
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`;
-}
+  function displayForecast(response) {
+    let forecast = response.data.daily;
 
-function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let forecastElement = document.querySelector("#forecast");
 
-  return days[day];
-}
-
-function displayForecast(response) {
-  let forecast = response.data.daily;
-
-  let forecastElement = document.querySelector("#forecast");
-
-  let forecastHTML = `<div class="row">`;
-  forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
-      forecastHTML =
-        forecastHTML +
-        `
+    let forecastHTML = `<div class="row">`;
+    forecast.forEach(function (forecastDay, index) {
+      if (index < 6) {
+        forecastHTML =
+          forecastHTML +
+          `
       <div class="col-2">
         <div class="weather-forecast-date">${formatDay(forecastDay.time)}</div>
         <img
@@ -58,74 +59,75 @@ function displayForecast(response) {
         </div>
       </div>
   `;
-    }
-  });
+      }
+    });
 
-  forecastHTML = forecastHTML + `</div>`;
-  forecastElement.innerHTML = forecastHTML;
-}
-function getForecast(coordinates) {
-  let apiKey = "c45f0a4aefb72ac935cfb59c3oe1ca4t";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayForecast);
-}
-function displayTemperature(response) {
-  console.log(response.data);
-  let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind");
-  let dateElement = document.querySelector("#date");
-  let iconElement = document.querySelector("#icon");
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+  }
+  function getForecast(coordinates) {
+    let apiKey = "c45f0a4aefb72ac935cfb59c3oe1ca4t";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+  }
+  function displayTemperature(response) {
+    console.log(response.data);
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
 
-  let celsiusTemperature = response.data.temperature.current;
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    let celsiusTemperature = response.data.temperature.current;
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
 
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature) + "°";
-  cityElement.innerHTML = response.data.city;
-  console.log(response.data.country);
-  descriptionElement.innerHTML = response.data.condition.description;
-  humidityElement.innerHTML =
-    "Humidity:" + " " + response.data.temperature.humidity + "%";
-  windElement.innerHTML =
-    "Wind:" + " " + Math.round(response.data.wind.speed) + " " + "km/h";
-  dateElement.innerHTML = formatDate(response.data.time);
-  getForecast(response.data.coordinates);
-  iconElement.setAttribute("src", response.data.condition.icon_url);
-  iconElement.setAttribute("alt", response.data.condition.description);
-  iconElement.setAttribute("width", "60");
-  getForecast(response.data.coordinates);
-}
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature) + "°";
+    cityElement.innerHTML = response.data.city;
+    console.log(response.data.country);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML =
+      "Humidity:" + " " + response.data.temperature.humidity + "%";
+    windElement.innerHTML =
+      "Wind:" + " " + Math.round(response.data.wind.speed) + " " + "km/h";
+    dateElement.innerHTML = formatDate(response.data.time);
+    getForecast(response.data.coordinates);
+    iconElement.setAttribute("src", response.data.condition.icon_url);
+    iconElement.setAttribute("alt", response.data.condition.description);
+    iconElement.setAttribute("width", "60");
+    getForecast(response.data.coordinates);
+  }
 
-function search(city) {
-  let apiKey = "c45f0a4aefb72ac935cfb59c3oe1ca4t";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?&query=${city}&key=${apiKey}&units=metric`;
+  function search(city) {
+    let apiKey = "c45f0a4aefb72ac935cfb59c3oe1ca4t";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?&query=${city}&key=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(displayTemperature);
-  console.log("search function called with city: " + city);
-}
-function handleSubmit(event) {
-  event.preventDefault();
-  let cityInputElement = document.querySelector("#getCity");
-  search(cityInputElement.value);
-}
+    axios.get(apiUrl).then(displayTemperature);
+    console.log("search function called with city: " + city);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#getCity");
+    search(cityInputElement.value);
+  }
 
-function handlePosition(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
+  function handlePosition(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
 
-  let apiKey = "c45f0a4aefb72ac935cfb59c3oe1ca4t";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
+    let apiKey = "c45f0a4aefb72ac935cfb59c3oe1ca4t";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(displayTemperature);
-}
+    axios.get(apiUrl).then(displayTemperature);
+  }
 
-function getCurrentLocation() {
-  navigator.geolocation.getCurrentPosition(handlePosition);
-}
+  function getCurrentLocation() {
+    navigator.geolocation.getCurrentPosition(handlePosition);
+  }
 
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", handleSubmit);
+  let form = document.querySelector("#search-form");
+  form.addEventListener("submit", handleSubmit);
 
-getCurrentLocation();
+  getCurrentLocation();
+});
